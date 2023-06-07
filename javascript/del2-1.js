18b4ad76181012554fc6a78bb2af1e0f38198293
Thenspe <91274102+Thenspe@users.noreply.github.com>
2023-06-07 11:36:01 -0400

    attempting to fix the geojson error

---
+++ javascript/del2-1.js
@@ -0,0 +1,29 @@
+// Initialize the map
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+// Add a tile layer (e.g., OpenStreetMap)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Load the GeoTIFF image
+var url = '/imagery/A19865-167_cog.tif';
+fetch(url)
+  .then(function(response) {
+    return response.arrayBuffer();
+  })
+  .then(function(arrayBuffer) {
+    // Create a GeoRasterLayer using the GeoTIFF data
+    var geoRasterLayer = new GeoRasterLayer({
+      url: url,
+      arrayBuffer: arrayBuffer,
+      pane: 'tilePane',
+      opacity: 0.7
+    });
+
+    // Add the GeoRasterLayer to the map
+    geoRasterLayer.addTo(map);
+
+    // Fit the map view to the GeoRasterLayer bounds
+    map.fitBounds(geoRasterLayer.getBounds());
+  });
