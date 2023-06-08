567d88cabc04c3bc2470d5dbd006c2d725809812
Thenspe <91274102+Thenspe@users.noreply.github.com>
2023-06-08 13:10:28 -0400

    completed the geoJSON.py file for now

--- createGeoJSON.py
+++ createGeoJSON.py
@@ -2,24 +2,29 @@ import arcpy
 import csv
 import json
 import glob
+import os
 
+# set current working directory
+current_directory = os.getcwd()
+print("Current working directory:",current_directory)
 # Path to the shapefile
-shapefile_path = "D:/FlemSem3/Collab/P2306/Del2/imagery/COGsForGeoJSON2.shp"
+shapefile_path = os.path.join(current_directory,"imagery","COGsForGeoJSON2.shp") # change to path and shapefile as required
+print("Shapefile absolute path: ",shapefile_path)
 
 # Path to the CSV file
-csv_file_path = "D:/FlemSem3/Collab/TrentUniFreeTIFs/Trent.csv"
+csv_file_path = os.path.join(current_directory,"imagery","TrentData.csv") # change path and csv name as required
+print("Shapefile absolute path:", csv_file_path)
 
 # path to the geoTIFFs/COGs
-img_file_path = "D:/FlemSem3/Collab/TrentUniFreeTIFs/"
-# create empy list to store the file names in
-tiffs = []
+img_file_path = os.path.relpath("imagery")  # change to the folder where the images are
+print("Shapefile absolute path:",img_file_path)
 
 # Field names in the shapefile and CSV file for joining
 shapefile_id_field = "PHOTO_ID"
 csv_id_field = "PHOTO_ID"   ########################## Cambium's is "x"
 
 # Path to the output GeoJSON file
-output_geojson_path = "D:/FlemSem3/Collab/P2306-Final-Website/imagery/aerialsPy.json"   # output name
+output_geojson_path = "imagery/aerialsPy.json"   # output name and location
 
 # Open the CSV file and create a dictionary based on the PHOTO_ID field
 csv_dict = {}
@@ -42,8 +47,9 @@ with arcpy.da.SearchCursor(shapefile_path, ["SHAPE@", shapefile_id_field]) as cu
         # Get the corresponding row from the CSV data
         csv_row = csv_dict.get(str(photo_id))
         # get the corresponding file name from the image folder
-        print("photo_id: ",photo_id)
-        imgFiles = glob.glob(img_file_path+str(photo_id)+'_*.tif')  ############################################
+        imgFile = None
+        for eachPic in glob.glob(os.path.join(img_file_path,str(photo_id)+'_*.tif')):  ############################################
+            imgFile = eachPic     
 
         if csv_row is not None:
             # Extract coordinates from the geometry
@@ -61,7 +67,7 @@ with arcpy.da.SearchCursor(shapefile_path, ["SHAPE@", shapefile_id_field]) as cu
             # construct the properties of the geoJSON feature
             properties = {
                 "PHOTO_ID":photo_id,
-                "File_Name":imgFiles
+                "File_Name":imgFile
             }
             # print(str(tiffs[photo_id-1]))
             # add all other key-value pairs from the csv
@@ -85,12 +91,10 @@ geojson = {
     "type": "FeatureCollection",
     "features": features
 }
-
 # Convert to JSON string
 geojson_str = json.dumps(geojson)
 
 # Save to a file
 with open(output_geojson_path, "w") as output_file:
     output_file.write(geojson_str)
-print(tiffs)
 print("Process complete.")
