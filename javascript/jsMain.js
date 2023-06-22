a2e8214cbc2621d4c9c943c8b5b2e5144a3c76e2
Thenspe <91274102+Thenspe@users.noreply.github.com>
2023-06-22 02:21:30 -0400

    Added all javascript dependencies. Cleaned up repo

--- javascript/jsMain.js
+++ javascript/jsMain.js
@@ -1,15 +1,18 @@
-//  Javascript function for navbar and responsive design 
-
-function navFunction() {
-    // var x = document.getElementsByTagName("nav");
-    var x = document.getElementById("hamburger");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+var resize = function() {
+    if($('body').width() < 800) {
+        $('#home').html('Home');
+        $('#del1').html('Del 1<br>WWIS');
+        $('#del2').html('Del 2<br>App');
+        $('#del2info').html('Del 2<br>Method');
+        $('#team').html('Team');
+    }
+    else {
+        $('#home').html('Streamlining GIS Processes<br>for Cambium Inc.');
+        $('#del1').html('Deliverable 1<br>Water Well Information System');
+        $('#del2').html('Deliverable 2<br>Aerial Image Application');
+        $('#del2info').html('Deliverable 2<br>Methodology');
+        $('#team').html('Meet the<br>Team');
     }
 }
-function menuClick() {
-    document.getElementByClassName("subMenu").style.backgroundColor = "orange";
-}
-document.getElementById("menuButton").addEventListener("click", menuClick);
+$(document).ready(resize);
+$(window).on('resize',resize);
