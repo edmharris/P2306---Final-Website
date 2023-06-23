e971e96c7c99c0cef59ca23d36df4a1d291c4b8f
Thenspe <91274102+Thenspe@users.noreply.github.com>
2023-06-22 22:56:33 -0400

    javascript for slideshow

--- javascript/jsMain.js
+++ javascript/jsMain.js
@@ -1,3 +1,4 @@
+// This function renames the navigation buttons based on screen size
 var resize = function() {
     if($('body').width() < 800) {
         $('#home').html('Home');
@@ -16,3 +17,22 @@ var resize = function() {
 }
 $(document).ready(resize);
 $(window).on('resize',resize);
+
+// this set is for the del1 slideshow as per https://www.w3schools.com/w3css/w3css_slideshow.asp
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideshow");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
